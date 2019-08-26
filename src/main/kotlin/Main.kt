8dc3db0d8646f399f33a9f3ceb741a6e1a7aff0d
therealfarfetchd <git@therealfarfetchd.dev>
2019-08-26 14:35:26 +0200

    Gradle

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,9 @@
 import tokenizer.Tokenizer
-import java.io.File
 
 fun main(args: Array<String>) {
     val unformatted =
-        File("C:\\Development\\Kotlin\\Tokenizer\\src\\files\\page0.md").inputStream().bufferedReader().readLines()
+        (ClassLoader.getSystemClassLoader().getResourceAsStream("files/page0.md")
+            ?: error("Couldn't load files/page0.md")).bufferedReader().readLines()
             .joinToString("\n")
 
     val paragraphs = Tokenizer.tokenize(unformatted)
